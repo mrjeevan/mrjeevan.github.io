afa925fc5f413121b07125e82017d4467714dac8
mrjeevan <mrjeevan2000@gmail.com>
2020-08-10 22:14:27 +0530

    Testing-2.0

---
+++ js/landing.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("p").hover(function(){
+      $(this).html("Student, Python Developer and more ... ");
+      }, function(){
+      $(this).html("M R Jeevan - 2020");
+    });
+  });

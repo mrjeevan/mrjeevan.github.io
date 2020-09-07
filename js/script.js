3d9dc9b6e49ade4bed3c0a0bb712a5ecc12eb98a
mrjeevan <mrjeevan2000@gmail.com>
2020-09-07 11:56:20 +0530

    formating

--- js/script.js
+++ js/script.js
@@ -1,41 +1,19 @@
 function more(){
     alert("\nstep 1 : Get a ideia \nstep 2 : Implement it \nstep 3 : Get it working \nStuck @ step 3;\n \n  Will be avilable soon ;)");
 }
-
-
-function setCookie(cname, cvalue) {
-   
-    document.cookie = cname + "=" + cvalue + ";" ;
-  }
-
-alert('setting cookie');
-setcookie("mode","dark");
-alert('cookie set');
-
-
-
 function dark()
 {
-    
     var b = document.getElementsByTagName('body')[0];
-    
     b.style.backgroundColor = 'black';
-    
     b.style.backgroundImage = "url('resource/dark-background-wallpaper.jpg')";
-    
     b.style.color = 'azure';
     b.style.backgroundAttachment = 'fixed';
     b.style.backgroundSize = 'cover';
-    
-        
-    
-
     // invert button part 
     $("#mode").html("light mode!");
     $("#mode").attr("onclick","light();");
     $("#mode").attr("class","mode btn btn-light ");
     $("a").css("color","orange");
-
     // Nav-Hover-Fix
     $(".nav-link").hover(function(){
         $(this).css("color","red");
@@ -57,38 +35,25 @@ function dark()
 function light()
 {
     // alert('Debug : Light Mode!')
-
     var b = document.getElementsByTagName('body')[0];
-    
     // test
     b.style.backgroundColor = 'thistle';
-    
     b.style.backgroundImage = "url('resource/White-Wallpaper-For-Pc.jpg')";
-
     b.style.color = 'black';
     b.style.backgroundAttachment = 'fixed';
     b.style.backgroundSize = 'cover';
-
-
-     
-
     // invert button part 
     $("#mode").html("Dark mode!");
     $("#mode").attr("onclick","dark();");
     $("#mode").attr("class","mode btn btn-dark ");
     $("a").css("color","royalblue");
-
     // Nav-Hover-Fix
     $(".nav-link").hover(function(){
         $(this).css("color","red");
     },function(){
         $(this).css("color","royalblue");
     });
-
-
 }
-
-
 //   background-image: url('https://www.fg-a.com/wallpapers/white-wave-pattern.jpg');
 //   background-attachment: fixed;
 //   background-size: cover;
